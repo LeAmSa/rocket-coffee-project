ed22ad2711ee1d75aed91802bd781145278e6b7c
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-06-29 11:12:27 -0300

    Add nav links animation

--- scripts/mobile-navbar.js
+++ scripts/mobile-navbar.js
@@ -1,5 +1,16 @@
 let i = 0
 
+function navLinkAnimation() {
+    const navLinks = document.querySelectorAll('li')
+    navLinks.forEach(function(link) {
+        /*Verificando se existe a propriedade animation em cada li no html. Se já existe, deixa vazio. Se não, cria.*/
+        link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`)
+    })
+    
+}
+
 function activeMobileMenu() {
     const navbar = document.querySelector('ul')
     const imgNavbar = document.getElementById('mobile-menu-open')
@@ -10,4 +21,5 @@ function activeMobileMenu() {
     } else {
         imgNavbar.src = 'img/menu-buguer-close.svg'
     }
+    navLinkAnimation()
 }
