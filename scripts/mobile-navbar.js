fae56a7379e4916c11ad6eb7856df840ec6081d9
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-06-28 21:45:50 -0300

    Add menu burguer swap function

--- scripts/mobile-navbar.js
+++ scripts/mobile-navbar.js
@@ -1,6 +1,13 @@
+let i = 0
+
 function activeMobileMenu() {
     const navbar = document.querySelector('ul')
     const imgNavbar = document.getElementById('mobile-menu-open')
     navbar.classList.toggle('active')
-    imgNavbar.src = 'img/menu-buguer-close.svg'
+    i++
+    if (i % 2 == 0) {
+        imgNavbar.src = 'img/menu-buguer-open.svg'
+    } else {
+        imgNavbar.src = 'img/menu-buguer-close.svg'
+    }
 }
